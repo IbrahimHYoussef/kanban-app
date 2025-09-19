bbc9d67f32324d04acc506ebf34229659b256738
IbrahimHYoussef <ibrahimhyoussef18@gmail.com>
2025-09-20 00:30:11 +0300

    added layout for auth routes

---
+++ kanban-front/src/routes/(public)/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  {@render children()}
+</div>
