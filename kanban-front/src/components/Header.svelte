5faffa4f0f989df2da3fe991b12c26e3f52eec2f
IbrahimHYoussef <ibrahimhyoussef18@gmail.com>
2025-10-04 12:16:35 +0300

    removed svelte project

--- kanban-front/src/components/Header.svelte
+++
@@ -1,8 +0,0 @@
-
-
-<div>
-    <img src="" alt="">
-
-    <a href="/about">About</a>
-    <a href="/login">login</a>
-</div>
--- kanban-front/src/routes/(public)/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  let { children } = $props();
-</script>
-
-<div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-  {@render children()}
-</div>
